60b99b6616e7ee1a676cf668b5345ebe6a72be4d
JRD8 <jdforbew@gmail.com>
2016-05-10 16:44:12 -0400

    chore: propose final before quiz submission/prior to lint

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,49 +1,5 @@
 /* $("#main").append("The Great Houdiener"); */
 
-var skills = ["bandleader", "prankster", "bongoist"];
-
-var bio = {
-	"name" : "The Great JRD",
-	"role" : "Master Magiciano",
-	"contact_info" : "800-jrd-ring",
-	"picture_URL" : "images/me.jpg",
-	"welcome_message" : "aha, you are now entering my lair...",
-	"skills" : skills 
-};
-
-
-var work = {};
-work.start = "1991";
-work.employer = "Funky Claude";
-work.position = "Mahavishnu";
-work.dates = "1991 - 1998";
-work.location = "Timbuktu";
-work.desc = "provider of laughs";
-
-var education = {
-	"schools" : 
-	[
-		{
-			"name" : "Wash University",
-			"degree" : "MBA",
-			"grad_date" : "1990",
-			"city" : "Sahara Dessert",
-			"online" : "Udacity",
-			"majors" : ["mining", "card sharping"],
-			"minors" : ["livin", "lovin", "just like a bloodhound"]
-		},
-		{
-			"name" : "JH University",
-			"degree" : "BS",
-			"grad_date" : "1988",
-			"city" : "Bamore",
-			"online" : "Coursera",
-			"majors" : ["econ", "sleeping"],
-			"minors" : []
-		}
-	]
-}
-
 // var formattedName = HTMLheaderName.replace("%data%", bio.name);
 // var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 // var formattedContact = HTMLmobile.replace("%data%", bio.contact_info);
@@ -60,6 +16,104 @@ $("#main").append(education.name);
 $("#main").append(work["position"]);
 
 // STARTING ANEW HERE...
+
+var work = {
+	"job" :
+	[
+		{
+			"employer" : "The Royal Mounties",
+			"title" : "CEO",
+			"location" : "Galaxy Far Far Away",
+			"dates" : "1978 - 1965",
+			"desc" : "did alot of hard work"
+		},
+		{
+			"employer" : "Ripley's",
+			"title" : "Archivist",
+			"location" : "Orlando",
+			"dates" : "200 BC - 1965",
+			"desc" : "saw amazing stuff"
+		}
+	] 
+};
+
+var projects = {
+	"project" : 
+	[
+		{
+			"title" : "pyramid excavation",
+			"dates" : "1999 - present",
+			"desc" : "alot of sweating",
+			"image" : "images/me.jpg"
+		},
+		{
+			"title" : "magic convention",
+			"dates" : "2050 - present",
+			"desc" : "alot of trickery",
+			"image" : "images/me2.png"
+		},
+		{
+			"title" : "airline convention",
+			"dates" : "2040 - 2050",
+			"desc" : "alot of flying",
+			"image" : "images/fry.jpg"
+		}
+	]
+};
+
+var bio = {
+	"name" : "The Great JRD",
+	"role" : "Master Magiciano",
+	"welcome" : "aha, you are now entering my lair...",
+	"contacts" : 
+	{
+		"mobile" : "800-jrd-ring",
+		"email" : "abd@abada.com",
+		"github" : "jafa9",
+		"twitter" : "@funny",
+		"location" : "NYC"
+	},
+	"skills" : ["bandleader", "prankster", "bongoist"]
+};
+
+var education = {
+	"schools" : 
+	[
+		{
+			"name" : "Wash University",
+			"location" : "Sahara Dessert",
+			"degree" : "MBA",
+			"majors" : ["mining", "card sharping"],
+			"dates" : "1990 - 1992",
+			"url" : "www.coyote.com"
+		},
+		{
+			"name" : "JH University",
+			"location" : "Bamore",
+			"degree" : "BS",
+			"majors" : ["econ", "sleeping"],
+			"dates" : "1988 - 2000",
+			"url" : "www.laughing.com"
+		}
+	],
+
+	"onlineCourses" :
+	[
+		{
+			"title" : "Advanced Learning",
+			"school" : "Udacity",
+			"dates" : "5787 - 5600 BC",
+			"url" : "www.udacity.com"
+		},
+		{
+			"title" : "Advanced Running",
+			"school" : "Coursera",
+			"dates" : "1967 - 2010",
+			"url" : "www.coursera.com"
+		}
+	]
+}
+
 
 
 
